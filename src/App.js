c7c8ce8e2ab6591231925be42f14f6fcb05d56a3
Viacheslav Shamreliuk <viacheslav@Viacheslavs-MacBook-Pro.local>
2021-04-03 13:29:32 +0300

    create vazualize data structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { Graph } from "react-d3-graph";
+import config from './data-structure.json'
 
 function App() {
+  const data = { nodes: [], links: [] }
+  
+  // the graph configuration, just override the ones you need
+  const myConfig = {
+    nodeHighlightBehavior: true,
+    node: {
+      color: "lightgreen",
+      size: 120,
+      highlightStrokeColor: "blue",
+    },
+    link: {
+      highlightColor: "lightblue",
+    },
+  };
+  
+  const onClickNode = function(nodeId) {
+    window.alert(`Clicked node ${nodeId}`);
+  };
+  
+  const onClickLink = function(source, target) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };
+
+  const addNewNode = (name) => {
+    data.nodes.push({id: name})
+  }
+  const addNewLink = (source, target) => {
+    data.links.push({ source: source, target:target })
+  }
+
+  config.forEach(item => {
+    item?.name && addNewNode(item?.name)
+
+    item?.fields?.length && item.fields.forEach(field => {
+      field?.name && addNewNode(field?.name)
+    })
+  })
+
+  config.forEach(item => {
+    item?.fields?.length && item.fields.forEach(field => {
+      addNewLink(item?.name, field?.name)
+      if (field?.name && field?.relationship && data.nodes.filter(({ id }) => id === field?.relationship).length)
+        addNewLink(field?.name, field?.relationship)
+     
+    })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Graph
+        id="graph-id" // id is mandatory
+        data={data}
+        config={myConfig}
+        onClickNode={onClickNode}
+        onClickLink={onClickLink}
+      />
     </div>
   );
 }
